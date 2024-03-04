893abbee032310b957e82f8d25f7c7c85293c5b8
warllus <116952032+warllus@users.noreply.github.com>
2024-03-05 01:08:32 +0330

    Add files via upload

---
+++ tamrin 2/tamrin 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MyProgram
+{
+    static void Main()
+    {
+        Console.Write("Please enter the value of x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate the expression using the left shift operator instead of multiplication
+        double y = ((31 << 1) - 17) * x;
+
+        Console.WriteLine($"The value of the expression is: {y}");
+    }
+}
