223ea80642ed31d0f9252ea4359428dc77065f1e
warllus <116952032+warllus@users.noreply.github.com>
2024-03-05 00:22:59 +0330

    Add files via upload

---
+++ tamrin 1/tamrin 1/tamrin 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter a number: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Multiples of 3 less than {userInput}:");
+        for (int i = 3; i < userInput; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
