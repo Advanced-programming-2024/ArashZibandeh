a7b274330c3b634f622cc47c76269f19335c0784
warllus <116952032+warllus@users.noreply.github.com>
2024-03-12 01:20:31 +0330

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+        do
+        {
+            Console.Write("Please enter an odd number: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Error: Please enter an odd number. Try again.");
+            }
+            else
+            {
+                int result = HesabSeriesSum(n);
+                Console.WriteLine($"The sum of the series is: {result}");
+            }
+        } while (n % 2 == 0);
+    }
+
+    static int HesabSeriesSum(int n)
+    {
+        int sum = 0;
+        int sign = 1;
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            sum += sign * i;
+            sign = -sign; // Change sign in each step
+        }
+
+        return sum;
+    }
+}
