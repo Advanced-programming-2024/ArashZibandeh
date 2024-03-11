3e1cc33f72dd14907572d51120a29f194050ffbb
warllus <116952032+warllus@users.noreply.github.com>
2024-03-12 01:22:40 +0330

    Add files via upload

---
+++ Tamrin 2-2/Tamrin 2-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the base (x): ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the exponent (y): ");
+        int y = int.Parse(Console.ReadLine());
+
+        double result = CalculatePower(x, y);
+
+        Console.WriteLine($"{x}^{y} = {result}");
+    }
+
+    static double CalculatePower(int x, int y)
+    {
+        return Math.Pow(x, y);
+    }
+}
