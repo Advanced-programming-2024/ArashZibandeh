25d4da9bf7c850dd13f11097813cea80bafc3b64
warllus <116952032+warllus@users.noreply.github.com>
2024-05-14 16:49:09 +0330

    Add files via upload

---
+++ tamrin 4-30/tamrin 4-30/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Numbers between 1000 and 1100 divisible by 9 and their digit sum divisible by 9:");
+
+        for (int i = 1000; i <= 1100; i++)
+        {
+            if (i % 9 == 0 && DigitSum(i) % 9 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    // Function to calculate the sum of digits of a number
+    static int DigitSum(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
