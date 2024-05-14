0471777f029e054fcf2d8c121ad2e7b60d0515d1
warllus <116952032+warllus@users.noreply.github.com>
2024-05-14 16:51:16 +0330

    Add files via upload

---
+++ tamrin 4-24/tamrin 4-24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a number:");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("The number is palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not palindrome.");
+        }
+    }
+
+    static bool IsPalindrome(string number)
+    {
+        int length = number.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (number[i] != number[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
